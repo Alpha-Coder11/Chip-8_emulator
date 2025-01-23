016957a683a81161b1c723c8006cf15d2fea439f
Qazi <qazimashhood11@gmail.com>
2025-01-23 22:03:10 +0500

    chore(comments)

--- src/chip8_keyboard.c
+++ src/chip8_keyboard.c
@@ -1,16 +1,23 @@
 #include <assert.h>
 #include "chip8_keyboard.h"
-
+/*
+* Check if the key is in bound.
+*/
 static void chip8_keyboard_key_in_bound (uint8_t key)
 {
     assert(key < CHIP8_TOTAL_KEYS);
 }
+
 
 void chip8_keybaord_set_map(struct_chip8_keyboard_t* sys_keyboard,  const uint8_t* keyboard_map)
 {
     sys_keyboard->keyboard_map = keyboard_map;
 }
 
+/*
+* It makes an map of keys available to the emulator to the keys available in the keyboard.
+* If the key is not found, it returns KEY_NOT_FOUND.
+*/
 uint8_t chip8_keyboard_map(struct_chip8_keyboard_t* sys_keyboard, uint8_t key)
 {
     uint8_t index = KEY_NOT_FOUND;
@@ -23,17 +30,28 @@ uint8_t chip8_keyboard_map(struct_chip8_keyboard_t* sys_keyboard, uint8_t key)
     }
     return index;
 }
-
+/*
+* Set the key to down.
+*/
 void chip8_keyboard_key_down(struct_chip8_keyboard_t* keyboard, uint8_t key)
 {
     chip8_keyboard_key_in_bound(key);
     keyboard->keyboard[key] = 1;
 }
+
+/*
+* Set the key to up.
+*/
 void chip8_keyboard_key_up(struct_chip8_keyboard_t* keyboard, uint8_t key)
 {
     chip8_keyboard_key_in_bound(key);
     keyboard->keyboard[key] = 0;
 }
+
+/*
+* Checks if the key is down.
+* Return 1 if the key is down, 0 otherwise.
+*/
 uint8_t chip8_keyboard_is_key_down(struct_chip8_keyboard_t* keyboard, uint8_t key)
 {
     chip8_keyboard_key_in_bound(key);
--- src/chip8_memory.c
+++ src/chip8_memory.c
@@ -1,25 +1,37 @@
 #include <assert.h>
 #include "chip8_memory.h"
-
+/*
+* Check if the index is in bound of the memory
+*/
 static void chip8_memory_in_bound(uint32_t index)
 {
     assert( index < CHIP8_MEMORY_SIZE );
 }
 
+/*
+* Set the value at the index of the memory.
+*/
 void chip8_memory_set(struct_chip8_memory_t* sys_memory, uint32_t index, uint8_t value)
 {
     chip8_memory_in_bound(index);
     sys_memory->memory[index] = value; 
 }
 
+/*
+* Get the value at the index of the memory.
+*/
 uint8_t chip8_memory_get(struct_chip8_memory_t* sys_memory, uint32_t index)
 {
     chip8_memory_in_bound(index);
     return sys_memory->memory[index];
 }
+
+/*
+* Get the opcode at the index of the memory. Return the opcode as a 16 bit value.
+*/
 uint16_t chip8_memmory_get_opcode(struct_chip8_memory_t* sys_memory, uint32_t index)
 {
-    chip8_memory_in_bound(index + 1);   // this will not workin if index is at last value
+    chip8_memory_in_bound(index + 1);
     uint8_t msb = sys_memory->memory[index];
     uint8_t lsb = sys_memory->memory[index + 1];
     return (msb << 8) | lsb;
